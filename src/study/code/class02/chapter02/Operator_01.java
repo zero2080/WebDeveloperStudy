f1f133c2b031e394e41d5dd6cce2ab0ae109a247
ohbeomsoo <zero2080@naver.com>
2021-03-04 18:54:19 +0900

    master

--- src/study/code/class02/chapter02/Operator_01.java
+++ src/study/code/class02/chapter02/Operator_01.java
@@ -2,29 +2,27 @@ package study.code.class02.chapter02;
 
 public class Operator_01 {
     public static void main(String[] args) {
-        int a = 8;
-        int j = 3;
+        /**
+         * 단항연산자 예제
+         */
+        int num = 200;
+        int mNum = -num;    //변수가 음수라면 양수로 / 양수라면 음수로 전환된다.
+        int pNum = +mNum;    //변화없음 그냥 존재함(진짜로...진짜진자로...)
 
-        System.out.println("a = "+ a + " / j = "+j);
-        System.out.println("a 더하기 j 는 " + (a + j));
-        System.out.println("a 빼기 j 는 " + (a - j));
-        System.out.println("a 곱하기 j 는 " + a * j);
-        System.out.println("a 나누기 j의 몫은 " + a / j);
-        System.out.println("a 나누기 j의 나머지 는 " + a % j);
+        System.out.println(num);
+        System.out.println(mNum);   //양수가 음수로 전환됨
+        System.out.println(pNum);   //음수가 양수로 바뀌지도 않음
 
-        System.out.println("=======================================");
+        System.out.println(num);    //200
+        num++;                      // num에 1을 더함
+        System.out.println(num);    //201
+        num--;                      // num에서 1을 뺌
+        System.out.println(num);    //200
 
-        System.out.println("지금 a는 " + a);
-        System.out.println(" ++ 는 증가연산자 : " + (++a));   // 1을 1증가시키고 출력
-        System.out.println(" ++ 는 증가연산자 : " + (a++));   // 1을 출력하고 1증가
-        System.out.println("지금 a는 " + a);
+        System.out.println("후위연산 : " + num++);  //200출력   후위 연산   현제 행을 실행하고 해당 변수를 1증가시킴
+        System.out.println(num);    //201출력
+        System.out.println("전위연산 : " + ++num);  //202출력   전위 연산   현제 행을 실행하기 전에 해당변수에서 1을 감소시키고 실행
         
-        System.out.println("=======================================");
-
-        System.out.println("지금 a는 " + a);
-        System.out.println(" -- 는 감소연산자 : " + (--a));   // 1을 1감소시키고 출력
-        System.out.println(" -- 는 감소연산자 : " + (a--));   // 1을 출력하고 1감소
-        System.out.println("지금 a는 " + a);
 
     }
 }
